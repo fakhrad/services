eb971552ad92fc9bc35248498234c1f256af1a16
saeedPadyab <padyabsaeed@gmail.com>
2019-01-25 18:12:23 +0300

    added storage manager

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import themeManager from "./lib/themeManager";
 import languageManager from "./lib/languageManager";
+import storageManager from "./lib//storageManager";
 
-export {
-    themeManager,
-    languageManager
-}
+export { themeManager, languageManager, storageManager };
