ec5c16452653963d9ec02448e9d996fb44d91621
saeedPadyab <padyabsaeed@gmail.com>
2019-01-27 02:43:16 +0300

    authManager--stateManager,--navManager

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import themeManager from "./lib/themeManager";
 import languageManager from "./lib/languageManager";
 import storageManager from "./lib//storageManager";
+import navManager from "./lib/navManager";
+import stateManager from "./lib/stateManager";
+import authManager from "./lib/authManager";
 
-export { themeManager, languageManager, storageManager };
+export {
+  themeManager,
+  languageManager,
+  storageManager,
+  navManager,
+  stateManager,
+  authManager
+};
